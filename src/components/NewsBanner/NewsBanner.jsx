ab57ae15b16f88310ed98dc0fda9808a1c55ff61
Polina Savateeva <polina.savateeva@geogracom.com>
2024-04-09 16:42:25 +0300

    created first functions, using api and inizializing repo

---
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -0,0 +1,15 @@
+import Image from '../Image/Image'
+import styles from './styles.module.css'
+import { formatTimeAgo } from '../../helpers/formatTimeAgo'
+
+const NewsBanner = ({item}) => {
+  return (
+    <div className={styles.banner}>
+      <Image image={item?.image} />
+      <h3 className={styles.title}>{item.title}</h3>
+      <p className={styles.extra}>{formatTimeAgo(item.published)} by {item.author}</p>
+    </div>
+  )
+}
+
+export default NewsBanner;
